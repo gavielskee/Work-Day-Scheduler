60f3e17b1baed14d3ffc8ad895176f3a4a3197d5
gavielskee <gavielskee1@gmail.com>
2023-08-02 19:44:54 -0400

    shows current time in header

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-````// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
@@ -14,14 +14,17 @@ $(document).ready(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  var present = dayjs();
-  var past;
-  var future;
-
+  currentHour = dayjs().format('HH'); 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  $('#currentDay').text(dayjs().format());
+  
 });
+function displayTime () {
+  
+    $('#currentDay').text(dayjs().format('dddd, MMMM D YYYY, h:mm:ss a'));
+    
+    }
+setInterval(displayTime, 1000)
